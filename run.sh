188cf4781a377278e42703f440ccaf356fae0603
Imane219 <yiman420@foxmail.com>
2023-09-04 13:53:05 +0800

    update shell

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ config='./configs/train_rn12_cifar_clean.yaml'
 name='rn12/cifarfs/clean'
 
 
-# CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$gpu python train_basemodel_adversarially.py --config $config --name $name --gpu $gpu
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$gpu python train_basemodel.py --config $config --name $name --gpu $gpu
 
-CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$gpu python fs_test.py --config $config --name $name --gpu $gpu
+# CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$gpu python fs_test.py --config $config --name $name --gpu $gpu
