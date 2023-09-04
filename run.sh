33784182ffd86bdceebec796383029cef027802f
Imane219 <yiman420@foxmail.com>
2023-09-04 13:52:47 +0800

    update shell

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 set -x
 
 gpu=0
-config='./configs/fs_test_fc100.yaml'
-name='rn12-wide/fc100/at'
+config='./configs/train_rn12_cifar_clean.yaml'
+name='rn12/cifarfs/clean'
 
 
 # CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$gpu python train_basemodel_adversarially.py --config $config --name $name --gpu $gpu
 
-CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$gpu python fs_test_cw.py --config $config --name $name --gpu $gpu
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$gpu python fs_test.py --config $config --name $name --gpu $gpu
